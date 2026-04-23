827c3ee45618e0bbbb562ed5a3e584b50ece6ce2
hatake21373-ai <hatake21373@gmail.com>
2026-04-24 02:32:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=["GET", "POST"])
 def callback():
+    print("method:", request.method)
     data = request.get_json(silent=True)
     print(json.dumps(data, ensure_ascii=False, indent=2))
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
